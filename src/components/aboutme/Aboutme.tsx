727ddfb0488309416b88778f19d0a058892379c6
derick-asane <derickasane@gmail.com>
2024-07-07 23:56:56 +0100

    solve the responsiveness in projects

--- src/components/aboutme/Aboutme.tsx
+++ src/components/aboutme/Aboutme.tsx
@@ -1,5 +1,12 @@
 function Aboutme() {
-  return <div>About</div>;
+  return (
+    <div className="h-screen">
+      <div className="flex justify-center">
+        <h1>About Me</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Aboutme;
--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -30,22 +30,41 @@ const Project = () => {
       label:
         "If you’re just now becoming familiar with what a gradient is, we’ll paint the picture for you before we dive into the tougher stuff",
     },
+    {
+      id: 5,
+      link: "",
+      image: projectImage,
+      label:
+        "If you’re just now becoming familiar with what a gradient is, we’ll paint the picture for you before we dive into the tougher stuff",
+    },
+    ,
+    {
+      id: 6,
+      link: "",
+      image: projectImage,
+      label:
+        "If you’re just now becoming familiar with what a gradient is, we’ll paint the picture for you before we dive into the tougher stuff",
+    },
   ];
 
   return (
-    <div className="h-screen px-8">
+    <div className=" px-8" id="Project">
       <div className="flex justify-center pt-4">
-        <h1 className="text-black">Projects</h1>
+        <h1 className="text-black font-bold">Projects</h1>
       </div>
-      <div className="flex justify-around flex-wrap mt-8">
+      <div className="flex justify-around flex-wrap gap-2 mt-8">
         {Project.map((project, index) => (
-          <div key={index} className="w-[20%]">
-            <a href={project.link} className="a__remove-style">
+          <div key={index} className="W-[80%] mb-2 md:w-[25%]">
+            <a href={project?.link} className="a__remove-style">
               <div className="w-[100%] border rounded-lg">
-                <img src={project.image} alt="" className="object-contain" />
+                <img
+                  src={project?.image}
+                  alt={project?.label}
+                  className="object-fill w-[100%]"
+                />
               </div>
               <div className="bg-slate-900 flex justify-center">
-                <span className="text-white px-2">{project.label}</span>
+                <span className="text-white px-2">{project?.label}</span>
               </div>
             </a>
           </div>
